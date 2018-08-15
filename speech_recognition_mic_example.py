46245ed3fcfaa7c0b7139288a2cf51146c159412
outlaw6 <milan.damjanovic.mbs@gmail.com>
2018-08-15 14:55:46 +0200

    New files

--- speech_recognition_mic_example.py
+++ speech_recognition_mic_example.py
@@ -1,10 +1,12 @@
 import speech_recognition as sr
 
 recognizer = sr.Recognizer()
-audiofile = sr.AudioFile('speech.wav')
 
-with audiofile as source:
+sr.Microphone.list_microphone_names()
+mic = sr.Microphone(device_index=1)
+
+with mic as source:
     recognizer.adjust_for_ambient_noise(source)
-    audio = recognizer.record(source)
+    audio = recognizer.listen(source)
     
 print(recognizer.recognize_google(audio))
